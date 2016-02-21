39287c107dcbd7b5c2399712bd4ba09ed3dce2f2
TimMagoun <timmagoun@gmail.com>
2016-02-21 18:52:49 -0500

    Extensive comments and under the hood maintenance

--- src/org/usfirst/frc/team1923/robot/subsystems/PIDDriveTrainSubsystem.java
+++ src/org/usfirst/frc/team1923/robot/subsystems/PIDDriveTrainSubsystem.java
@@ -12,30 +12,30 @@ import org.usfirst.frc.team1923.robot.utils.Calculator;
 public class PIDDriveTrainSubsystem extends PIDSubsystem {
 	
 	private Timer timer;
-	private double timeOut = 1.0; //Default timeout when robot starts
-	private boolean manual = true;
+	private double timeOut = 3.0; //Default timeout when robot starts
+	private boolean manual = true;//Boolean to indicate if PID is in use
 	
-	private double oldLeftSpeed=0, oldRightSpeed=0;
+	private double oldLeftSpeed=0, oldRightSpeed=0;	//Used for smoothDrive
 	private static final double 
-	Pe = 0.000, Ie = 0.000, De = 0.000;
+	Pe = 0.000, Ie = 0.000, De = 0.000;	//TODO: Input those values
 	private static final double
 	ENC_DIST_TOLERANCE = 1.0, //Tolerance in distance units
-	WHEEL_CIRCUMFERENCE = 6*Math.PI,
+	WHEEL_CIRCUMFERENCE = 6*Math.PI,		//**** DISTANCE UNIT IN INCHES ****
 	DEFAULT_TIMEOUT = 3.0; //Default of 3 seconds
     public PIDDriveTrainSubsystem() {
     	super(Pe, Ie, De);
     	RobotMap.leftDriveEncoder.setDistancePerPulse(WHEEL_CIRCUMFERENCE/256.0);
     	RobotMap.rightDriveEncoder.setDistancePerPulse(WHEEL_CIRCUMFERENCE/256.0);
-    	this.getPIDController().setPID(Pe, Ie, De);
+    	this.getPIDController().setPID(Pe, Ie, De);	//Redundant?
     	this.setAbsoluteTolerance(ENC_DIST_TOLERANCE);
-    	this.setOutputRange(-1.0, 1.0);
-    	this.setInputRange(-200, 200);
-    	this.disable();
+    	this.setOutputRange(-1.0, 1.0);	//Make sure it's legal for talon power setting
+    	this.setInputRange(-200, 200);	//Setpoint distances???
+    	this.disable();	//Starts in manual mode (user controlled)
     	
-    	timer=new Timer();
+    	timer=new Timer();	//Used for timeout calculations
     }
     
-    private void setManual(boolean m)
+    private void setManual(boolean m)	//Enables or disables PID calculations
     {
     	if(m) {
     		manual=true;
@@ -51,11 +51,11 @@ public class PIDDriveTrainSubsystem extends PIDSubsystem {
     	setDefaultCommand(new TeleopCommand());
     }
     
-    public void manualDrive(double left, double right) {
+    public void manualDrive(double left, double right) {	//Overload
     	manualDrive(left, right, DEFAULT_TIMEOUT);
     }
 
-    public void manualDrive(double left, double right, double timeOut) {
+    public void manualDrive(double left, double right, double timeOut) {	//Teleop-ed smooth drive
     	setManual(true);
     	smoothDrive(left, right);
     	this.timeOut = timeOut;
@@ -63,7 +63,7 @@ public class PIDDriveTrainSubsystem extends PIDSubsystem {
 		this.timer.start();
     }
     
-    public void rawDrive(double l, double r) {
+    private void rawDrive(double l, double r) {		//Under the hood stuff
     	RobotMap.leftDriveOne.set(l);
     	RobotMap.leftDriveTwo.set(l);
     	RobotMap.leftDriveThree.set(l);
@@ -73,11 +73,11 @@ public class PIDDriveTrainSubsystem extends PIDSubsystem {
     	RobotMap.leftDriveThree.set(r);
     }
     
-    public void stop() {
+    public void stop() {	//Hammer time
     	rawDrive(0,0);
     }
     
-    public void smoothDrive(double left, double right){
+    private void smoothDrive(double left, double right){	//	Under the hood coalescing
 		// open loop bias based correction
 		oldLeftSpeed = Calculator.ease(left, oldLeftSpeed);
 		oldRightSpeed = Calculator.ease(right, oldRightSpeed);
@@ -88,7 +88,11 @@ public class PIDDriveTrainSubsystem extends PIDSubsystem {
 		rawDrive(left, right);
 	}
     
-    public void driveDistance(double dist, double timeOut)
+    public boolean isManual() {		//Getter for the private mode boolean
+    	return manual;
+    }
+    
+    public void driveDistance(double dist, double timeOut)		//Drives straight using PID
     {
     	resetEncoders();
     	setSetpoint(dist);
@@ -103,11 +107,11 @@ public class PIDDriveTrainSubsystem extends PIDSubsystem {
     	driveDistance(dist, DEFAULT_TIMEOUT);
     }
     
-    public void resetEncoders() {
+    private void resetEncoders() {		//Under the hood
     	RobotMap.leftDriveEncoder.reset();
     	RobotMap.rightDriveEncoder.reset();
     }
-    public boolean reachedTarget() {
+    public boolean reachedTarget() {	//Is it finished?
 		if (timer.get() > this.timeOut || this.onTarget()) {
 			this.disable();
 			manual = true;
@@ -118,12 +122,11 @@ public class PIDDriveTrainSubsystem extends PIDSubsystem {
 		return false;
 	}
     
-    
     protected double returnPIDInput() {	//Returns the average distance since last reset
     	return (RobotMap.leftDriveEncoder.getDistance()+RobotMap.rightDriveEncoder.getDistance())/2.0;
     }
     
-    protected void usePIDOutput(double output) {
+    protected void usePIDOutput(double output) {	//System calls this method to set the talons
     	if(!manual) {
     		smoothDrive(output, output);
     	}
