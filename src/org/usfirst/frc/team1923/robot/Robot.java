5ef537f812f62e221396e56a99d39c4753c4227e
Tim Magoun <timmagoun@gmail.com>
2017-01-19 19:24:51 -0500

    Updated to 2017 Control System

--- src/org/usfirst/frc/team1923/robot/Robot.java
+++ src/org/usfirst/frc/team1923/robot/Robot.java
@@ -1,6 +1,7 @@
 
 package org.usfirst.frc.team1923.robot;
 
+import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
@@ -9,77 +10,73 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 import org.usfirst.frc.team1923.robot.commands.*;
 import org.usfirst.frc.team1923.robot.subsystems.*;
-import org.usfirst.frc.team1923.robot.utils.*;
-
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.vision.USBCamera;
 
 public class Robot extends IterativeRobot {
-	
+
 	public static GearSubsystem gearSubsystem = new GearSubsystem();
 	public static DriveTrainSubsytem driveSubsystem = new DriveTrainSubsytem();
 	public static IntakeRollerSubsystem intakeRollerSubsystem = new IntakeRollerSubsystem();
 	public static IntakePistonSubsystem intakePistonSubsystem = new IntakePistonSubsystem();
-    public static ShooterPistonSubsystem shooterPistonSubsystem = new ShooterPistonSubsystem();
-    public static ShooterWheelSubsystem shooterWheelSubsystem = new ShooterWheelSubsystem();
-    public static DefensePistonSubsystem defensePistonSubsystem = new DefensePistonSubsystem();
+	public static ShooterPistonSubsystem shooterPistonSubsystem = new ShooterPistonSubsystem();
+	public static ShooterWheelSubsystem shooterWheelSubsystem = new ShooterWheelSubsystem();
+	public static DefensePistonSubsystem defensePistonSubsystem = new DefensePistonSubsystem();
 
 	public static OI oi;
 
-    public Command autonomousCommand;
-    public Command teleopCommand;
-    public SendableChooser chooser;  
-    public CameraServer server;
-    
-    public USBCamera camera;
-    
-    
-    public void robotInit() {    	
-		oi = new OI(); 
+	public Command autonomousCommand;
+	public Command teleopCommand;
+	public SendableChooser chooser;
+	public CameraServer server;
+
+	public UsbCamera camera;
+
+	public void robotInit() {
+		oi = new OI();
 		teleopCommand = new TeleopCommand();
 		chooser = new SendableChooser();
-		
-		initializeAutons();//make auto
-		defaultRobotPosition(); //force robot into match config
-		initCamera();//start camera feed
-    }
-    
-    public void initializeAutons(){
-    	chooser.addDefault("Cross Defense", new AUTON_cross_defense());
+
+		initializeAutons();// make auto
+		defaultRobotPosition(); // force robot into match config
+		initCamera();// start camera feed
+	}
+
+	public void initializeAutons() {
+		chooser.addDefault("Cross Defense", new AUTON_cross_defense());
 		chooser.addObject("Starvation", new AUTON_starvation());
 		chooser.addObject("Take the L", new AUTON_nothing());
 		log();
-		//SmartDashboard.putData("Auton Chooser: ", chooser);
-    }
-    
-    public void defaultRobotPosition(){
-    	gearSubsystem.shiftDown(); //forces start in low gear
-		intakePistonSubsystem.intakeUp(); //force intake to go up
-		shooterPistonSubsystem.shooterDown(); //force shooter down
-		defensePistonSubsystem.up(); //bring defense pistons up
-		RobotMap.mainCompressor.setClosedLoopControl(true); //turn compressor on
-    }
-    
-    public void initCamera(){
-    	camera = new USBCamera("cam0");//cam1 for knightmare
-    	//camera.setFPS(20);
-    	//camera.updateSettings();
-    	
-    	server = CameraServer.getInstance();
-    	server.setQuality(50);
-    	server.startAutomaticCapture(camera);	
-    }
+		// SmartDashboard.putData("Auton Chooser: ", chooser);
+	}
 
+	public void defaultRobotPosition() {
+		gearSubsystem.shiftDown(); // forces start in low gear
+		intakePistonSubsystem.intakeUp(); // force intake to go up
+		shooterPistonSubsystem.shooterDown(); // force shooter down
+		defensePistonSubsystem.up(); // bring defense pistons up
+		RobotMap.mainCompressor.setClosedLoopControl(true); // turn compressor
+															// on
+	}
 
-    public void disabledInit(){
-    	if (autonomousCommand != null)
+	public void initCamera() {
+		// camera = new UsbCamera();/ cam1 for knightmare
+		// camera.setFPS(20);
+		// camera.updateSettings();
+
+		// server = CameraServer.getInstance();
+		// server.setQuality(50);
+		// server.startAutomaticCapture(camera);
+	}
+
+	public void disabledInit() {
+		if (autonomousCommand != null)
 			autonomousCommand.cancel();
 		if (teleopCommand != null)
 			teleopCommand.cancel();
-		
-		//Disabled shifting to low, talons to break
-		
+
+		// Disabled shifting to low, talons to break
+
 		Robot.gearSubsystem.shiftDown();
 		RobotMap.leftDriveOne.enableBrakeMode(true);
 		RobotMap.leftDriveTwo.enableBrakeMode(true);
@@ -87,81 +84,85 @@ public class Robot extends IterativeRobot {
 		RobotMap.rightDriveOne.enableBrakeMode(true);
 		RobotMap.rightDriveTwo.enableBrakeMode(true);
 		RobotMap.rightDriveThree.enableBrakeMode(true);
-		
-//		camera.stopCapture();
-//		camera.closeCamera();
-		
-    }
-	
+
+		// camera.stopCapture();
+		// camera.closeCamera();
+
+	}
+
 	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
 	}
 
-    public void autonomousInit() {
-        autonomousCommand = (Command) chooser.getSelected();
-        autonomousCommand.start();
-    }
+	public void autonomousInit() {
+		autonomousCommand = (Command) chooser.getSelected();
+		autonomousCommand.start();
+	}
 
-    public void autonomousPeriodic() {
-        Scheduler.getInstance().run();
-    }
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+	}
 
-    public void teleopInit() {
-        if (autonomousCommand != null) autonomousCommand.cancel();
-    }
+	public void teleopInit() {
+		if (autonomousCommand != null)
+			autonomousCommand.cancel();
+	}
 
-    /**
-     * This function is called periodically during operator control
-     */
-    
-    public void teleopPeriodic() {
-        Scheduler.getInstance().run();
-        //hoodManagement();
-        log();
-    }
-    
-    public void hoodManagement(){
-    	if(RobotMap.shooterEncoder.getRate() > 600)
-    		Robot.shooterPistonSubsystem.shooterUp();
-    	else if(RobotMap.shooterEncoder.getRate() < 500)
-    		Robot.shooterPistonSubsystem.shooterDown();
-    }
-    
-    public void log(){
-    	SmartDashboard.putData("Auton Chooser: ", chooser);
-    	
-    	SmartDashboard.putNumber("left joy: ", oi.leftStick.getY());
-    	SmartDashboard.putNumber("right joy: ", oi.rightStick.getY());
-    	SmartDashboard.putBoolean("Low Gear: ", gearSubsystem.getGearPosition());	
-    	
-    	SmartDashboard.putString("Intake Roller Status: ", intakeRollerSubsystem.getIntakeState());
-    	SmartDashboard.putBoolean("Intake Down?: ", intakePistonSubsystem.intakePosition());
-    	SmartDashboard.putBoolean("Intake Switch 1: ", RobotMap.limitSwitch1.get());
-    	SmartDashboard.putBoolean("Intake Switch 2: ", RobotMap.limitSwitch2.get());
-    	
-    	SmartDashboard.putBoolean("Shooter Hood Up?: ", !shooterPistonSubsystem.position());
-    	SmartDashboard.putBoolean("READY?!: ", readyToShooter());
-    	SmartDashboard.putNumber("Shooter Encoder Rate: ", RobotMap.shooterEncoder.getRate());
-    	
-    	SmartDashboard.putNumber("RIGHT:" , RobotMap.shooterLeft.get());
-    	SmartDashboard.putNumber("LEFT: ", RobotMap.shooterRight.get());
-    	
-//    	SmartDashboard.putBoolean("call: ", oi.xboxController.rb.get());
-//    	SmartDashboard.putBoolean("response: ", Robot.intakeRollerSubsystem.getOverride());
-    	
-    	SmartDashboard.putBoolean("Defense Piston Down: ", defensePistonSubsystem.isDown());
-    	
-//    	SmartDashboard.putNumber("Right Drive Encoder: ", RobotMap.rightDriveEncoder.get());
-//    	SmartDashboard.putNumber("Left Drive Encoder: ", RobotMap.leftDriveEncoder.get());
-    }
-    
-    public boolean readyToShooter(){
-    	return RobotMap.shooterEncoder.getRate() > 675 && RobotMap.shooterEncoder.getRate() < 750 && !shooterPistonSubsystem.position();
-    }
-    
+	/**
+	 * This function is called periodically during operator control
+	 */
 
-    public void testPeriodic() {
-        LiveWindow.run();
-    }
-    
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+		// hoodManagement();
+		log();
+	}
+
+	public void hoodManagement() {
+		if (RobotMap.shooterEncoder.getRate() > 600)
+			Robot.shooterPistonSubsystem.shooterUp();
+		else if (RobotMap.shooterEncoder.getRate() < 500)
+			Robot.shooterPistonSubsystem.shooterDown();
+	}
+
+	public void log() {
+		SmartDashboard.putData("Auton Chooser: ", chooser);
+
+		SmartDashboard.putNumber("left joy: ", oi.leftStick.getY());
+		SmartDashboard.putNumber("right joy: ", oi.rightStick.getY());
+		SmartDashboard.putBoolean("Low Gear: ", gearSubsystem.getGearPosition());
+
+		SmartDashboard.putString("Intake Roller Status: ", intakeRollerSubsystem.getIntakeState());
+		SmartDashboard.putBoolean("Intake Down?: ", intakePistonSubsystem.intakePosition());
+		SmartDashboard.putBoolean("Intake Switch 1: ", RobotMap.limitSwitch1.get());
+		SmartDashboard.putBoolean("Intake Switch 2: ", RobotMap.limitSwitch2.get());
+
+		SmartDashboard.putBoolean("Shooter Hood Up?: ", !shooterPistonSubsystem.position());
+		SmartDashboard.putBoolean("READY?!: ", readyToShooter());
+		SmartDashboard.putNumber("Shooter Encoder Rate: ", RobotMap.shooterEncoder.getRate());
+
+		SmartDashboard.putNumber("RIGHT:", RobotMap.shooterLeft.get());
+		SmartDashboard.putNumber("LEFT: ", RobotMap.shooterRight.get());
+
+		// SmartDashboard.putBoolean("call: ", oi.xboxController.rb.get());
+		// SmartDashboard.putBoolean("response: ",
+		// Robot.intakeRollerSubsystem.getOverride());
+
+		SmartDashboard.putBoolean("Defense Piston Down: ", defensePistonSubsystem.isDown());
+
+		// SmartDashboard.putNumber("Right Drive Encoder: ",
+		// RobotMap.rightDriveEncoder.get());
+		// SmartDashboard.putNumber("Left Drive Encoder: ",
+		// RobotMap.leftDriveEncoder.get());
+	}
+
+	public boolean readyToShooter() {
+		return RobotMap.shooterEncoder.getRate() > 675 && RobotMap.shooterEncoder.getRate() < 750
+				&& !shooterPistonSubsystem.position();
+	}
+
+	public void testPeriodic() {
+		LiveWindow.run();
+	}
+
 }
--- src/org/usfirst/frc/team1923/robot/RobotMap.java
+++ src/org/usfirst/frc/team1923/robot/RobotMap.java
@@ -1,12 +1,14 @@
- package org.usfirst.frc.team1923.robot;
+package org.usfirst.frc.team1923.robot;
+
+import com.ctre.CANTalon;
 
 import edu.wpi.first.wpilibj.*;
 
 public class RobotMap {
 
-
 	public static final double EASE_INCREMENT = 0.09; // TODO test ease
-	public static final double DISTANCE_PER_PULSE = 1 / 256.0; //TODO get this value
+	public static final double DISTANCE_PER_PULSE = 1 / 256.0; // TODO get this
+																// value
 
 	// Drive
 	public static CANTalon leftDriveOne = new CANTalon(4);
@@ -15,54 +17,54 @@ public class RobotMap {
 	public static CANTalon rightDriveOne = new CANTalon(7);
 	public static CANTalon rightDriveTwo = new CANTalon(8);
 	public static CANTalon rightDriveThree = new CANTalon(9);
-//	public static Encoder rightDriveEncoder = new Encoder(2,3);
-//	public static Encoder leftDriveEncoder = new Encoder(4,5);
+	// public static Encoder rightDriveEncoder = new Encoder(2,3);
+	// public static Encoder leftDriveEncoder = new Encoder(4,5);
 
-    //Shooter
-    public static CANTalon shooterRight = new CANTalon(0);
-    public static CANTalon shooterLeft = new CANTalon(2);
-    public static Encoder shooterEncoder = new Encoder(4,5);
+	// Shooter
+	public static CANTalon shooterRight = new CANTalon(0);
+	public static CANTalon shooterLeft = new CANTalon(2);
+	public static Encoder shooterEncoder = new Encoder(4, 5);
 
 	// Intake
 	public static CANTalon intake = new CANTalon(3);
 	public static DigitalInput limitSwitch1 = new DigitalInput(0);
 	public static DigitalInput limitSwitch2 = new DigitalInput(1);
-	
-	//Pneumatics 
+
+	// Pneumatics
 	public static Compressor mainCompressor = new Compressor(10);
-	public static DoubleSolenoid shifterSolenoid = new DoubleSolenoid(10,6,7);
-	public static DoubleSolenoid intakeSolenoid = new DoubleSolenoid(10,2,3);
-	public static DoubleSolenoid shooterSolenoid = new DoubleSolenoid(10,4,5);
-	public static DoubleSolenoid defenseSolenoid = new DoubleSolenoid(10,0,1);
-	
+	public static DoubleSolenoid shifterSolenoid = new DoubleSolenoid(10, 6, 7);
+	public static DoubleSolenoid intakeSolenoid = new DoubleSolenoid(10, 2, 3);
+	public static DoubleSolenoid shooterSolenoid = new DoubleSolenoid(10, 4, 5);
+	public static DoubleSolenoid defenseSolenoid = new DoubleSolenoid(10, 0, 1);
+
 	// Robot initializing
 	public static void init() {
 		resetEncoders();
 		initializeTalons();
-		
+
 	}
-	
-	public static void resetEncoders(){
+
+	public static void resetEncoders() {
 		shooterEncoder.reset();
 		shooterEncoder.setDistancePerPulse(.5);
-		
-//		leftDriveEncoder.reset();
-//		leftDriveEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
-//		
-//		rightDriveEncoder.reset();
-//		rightDriveEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
+
+		// leftDriveEncoder.reset();
+		// leftDriveEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
+		//
+		// rightDriveEncoder.reset();
+		// rightDriveEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
 	}
-	
-	public static void initializeTalons(){
+
+	public static void initializeTalons() {
 		leftDriveOne.enableBrakeMode(true);
 		leftDriveTwo.enableBrakeMode(true);
 		leftDriveThree.enableBrakeMode(true);
 		rightDriveOne.enableBrakeMode(true);
 		rightDriveTwo.enableBrakeMode(true);
 		rightDriveThree.enableBrakeMode(true);
-		
-//		intake.enableBrakeMode(false);
-//		shooterLeft.enableBrakeMode(false);
-//		shooterRight.enableBrakeMode(false);
+
+		// intake.enableBrakeMode(false);
+		// shooterLeft.enableBrakeMode(false);
+		// shooterRight.enableBrakeMode(false);
 	}
 }
